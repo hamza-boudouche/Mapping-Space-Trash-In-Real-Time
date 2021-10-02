55e10c3ba0081dca0ae3f67324daf1ef75e382a1
walidmzough <wldmzough@gmail.com>
2021-10-03 00:51:57 +0100

    3d + 2d front-ends

--- 3d_globe/js/script.js
+++ 3d_globe/js/script.js
@@ -1,164 +1,33 @@
-import { OrbitControls } from "./OrbitControls.js"
-import { GLTFLoader } from "./GLTFLoader.js"
+import {OrbitControls} from "./OrbitControls.js"
+import {GLTFLoader} from "./GLTFLoader.js"
 import { Object3D, Vector3 } from "./three.module.js";
-
-
-const initialNum = 10
-const currentState = {
-	allDebris: [],
-	selectedDebris: null, // last clicked on debris
-	searchedDebris: [],
-}
-
-const fetchRandomDebris = async (count) => {
-	const res = await fetch(`http://localhost:5000/api/objects/${count}`)
-	const objects = await res.json()
-	return objects
-}
-var DebrisArray = fetchRandomDebris(50);
-
-var DebrisCoords = DebrisArray.map(debris => {
-
-	let coord = fetchDebrisData(debris.catalogNumber,new Date());
-	return({...debris,...coord})
-
-});
-currentState.allDebris = DebrisCoords;
-
-const fetchDebrisData = async (catalogNumber, date = new Date()) => {
-	const res = await fetch(`http://localhost:5000/api/orbit/${catalogNumber}/${date}`)
-	const data = await res.json()
-	return data
-}
-
-const autoSuggest = async (query) => {
-	const res = await fetch(`http://localhost:5000/api/objects/autoSuggest/${query}`)
-	const data = await res.json()
-	return data
-}
-
-const search = async (query) => {
-	const res = await fetch(`http://localhost:5000/api/objects/search/${query}`)
-	const data = await res.json()
-	return data
-}
-
-const renderInfo = () => {
-	// get html elements by id 
-	// render info
-}
-
-const onSubmitSearch = (e) => {
-	e.preventDefault()
-	const searchQuery = document.getElementById('searchField')
-	const debris = search(searchQuery)
-	currentState.searchedDebris = debris
-}
-
-const onChangeSearch = (e) => {
-	const searchQuery = document.getElementById('searchField')
-	const debris = autoSuggest(searchQuery)
-	currentState.searchedDebris = debris
-}
-
-const setUp = async () => {
-	currentState.selectedDebris = await fetchRandomDebris(initialNum)
-	currentDebris = currentState.selectedDebris[0]
-}
-
-/*setUp() 
-
-
-
-const myTree = new Quadtree({
-	x: 0,
-	y: 0,
-	width: 400,
-	height: 300
-});
-
-const myObjects = [];
-
-// for (var i = 0; i < 200; i = i + 1) {
-// 	myObjects.push({
-// 		x: randMinMax(0, 640),
-// 		y: randMinMax(0, 480),
-// 		width: randMinMax(10, 20),
-// 		height: randMinMax(10, 20),
-// 		vx: randMinMax(-0.5, 0.5),
-// 		vy: randMinMax(-0.5, 0.5),
-// 		check: false
-// 	});
-// }
-
-// function loop() {
-// 	//remove all objects and subnodes 
-// 	myTree.clear();
-// 	//update myObjects and insert them into the tree again
-// 	for (var i = 0; i < myObjects.length; i = i + 1) {
-// 		myObjects[i].x += myObjects[i].vx;
-// 		myObjects[i].y += myObjects[i].vy;
-// 		myTree.insert(myObjects[i]);
-// 	}
-// 	//call next frame
-// 	requestAnimationFrame(loop);
-// }
-
-// var myCursor = {
-// 	x: mouseX,
-// 	y: mouseY,
-// 	width: 20,
-// 	height: 20
-// };
-
-// var candidates = myTree.retrieve(myCursor);
-
-// const toCheck = []
-
-// for (var i = 0; i < candidates.length; i = i + 1) {
-// 	candidates[i]
-// }
-
-const fetchRandomDebrisCoords = async (objects) => {
-	const res = Promise.all(objects.map(obj => fetchDebrisData(obj.catalogNumber, new Date())))
-	return res
-}
-
-const checkForProbableCollissions = (objectsWithCoords) => {
-	const myTree = new Quadtree({
-		x: 0,
-		y: 0,
-		width: 400,
-		height: 300
-	});
-}
-*/
-
+console.log(document.getElementById('demo').value);
 
 class BlueMarble {
 
 constructor(withSun,withClouds,withGrid){
-	this.scene.background = this.space_texture;
+	//this.scene.background = this.space_texture;
 	this.renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( this.renderer.domElement );
 	this.controls.zoomSpeed=0.5;
 this.controls.rotateSpeed=0.5;  
-	this.scene.add( this.globe_sphere );
+this.globe.add( this.globe_sphere);
 	this.scene.add( this.amb_light );
-	this.scene.add(this.orbitals);
 	this.scene.add(this.orbits);
-	if (withClouds) this.scene.add( this.cloud_sphere );
+	if (withClouds) this.globe.add( this.cloud_sphere );
 	if(withSun)	{
 		this.light.position.set(5000,0,5000);
 		this.scene.add( this.light );
-		this.sun_animate();		
+			
 			}
+			this.scene.add( this.globe);
 		this.animate();
-		
 	}
+selected = false;
 scene = new THREE.Scene();
 camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
 renderer = new THREE.WebGLRenderer();
+globe = new THREE.Group();
 orbitals = new THREE.Group();
 orbits = new THREE.Group();
 addOrbital(object){
@@ -191,15 +60,15 @@ light = new THREE.PointLight( 0xffffff,1.4 , 0 ,2); // sun sim
 
 amb_light = new THREE.AmbientLight( 0xffffff,0.2 ); // soft white light
 // stary space
-loader = new THREE.CubeTextureLoader();
-space_texture = this.loader.load([
+//loader = new THREE.CubeTextureLoader();
+/*space_texture = this.loader.load([
   'resources/space_cubemap/stars.png',
   'resources/space_cubemap/stars.png',
   'resources/space_cubemap/stars.png',
   'resources/space_cubemap/stars.png',
   'resources/space_cubemap/stars.png',
   'resources/space_cubemap/stars.png',
-]);
+]);*/
 //camera mouse control and init pos
 controls = new OrbitControls( this.camera, this.renderer.domElement );
 
@@ -215,6 +84,19 @@ sun_animate = () => {
 
 }
 
+earthRot = () => {
+	requestAnimationFrame( this.earthRot );
+	this.globe.rotation.y +=0.01;
+
+
+}
+entrance = () => {
+	requestAnimationFrame( this.entrance );
+	
+	if (this.globe.position.x>0 ) {this.globe.position.x -= 0.25;this.camera.position.z+=0.1;this.globe.rotation.y +=0.04;}
+	else ( this.scene.add(this.orbitals))
+	
+}
 
 }
 
@@ -243,7 +125,8 @@ class Debris extends THREE.Mesh{
 			this.color=color;
 			this.geometry = new THREE.SphereGeometry( 0.05, 1, 2 );
 			this.material = new THREE.MeshStandardMaterial( { color: this.color } ) ;
-			this.orbit = new Orbit(1000,this.color);			
+			this.orbit = new Orbit(1000,this.color);	
+			this.scale.set(10,10,10);		
 		}
 
 setPosition = (vect) => {
@@ -253,13 +136,14 @@ setPosition = (vect) => {
 }
 
 var myMarble = new BlueMarble(true,true,true);
+myMarble.globe.position.x = 30;
 myMarble.camera.position.z = 80;
-currentState.allDebris.forEach(coord => myMarble.addOrbital(new Debris([coord.longitude,coord.lattitude,coord.height],0xffff00));
 
+myMarble.addOrbital(new Debris([0.593412,0.10472,6371],0xffff00));
 
 
 // Tooltip
-let INTERSECTED;
+let INTERSECTED,ACTIVE;
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2().set(100,100);
 const mouse_adapted = new THREE.Vector2().set(100,100);
@@ -291,9 +175,10 @@ function render() {
 		// on hover
 		if ( INTERSECTED != intersects[ 0 ].object ) {
 
-			if ( INTERSECTED ) {INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex )
-			myMarble.removeOrbit(INTERSECTED.orbit);}
-			;
+			if (INTERSECTED) {INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+				if (!INTERSECTED.selected) myMarble.removeOrbit(INTERSECTED.orbit)
+			}
+			
 
 			 // highlight
 			INTERSECTED = intersects[0].object;
@@ -310,42 +195,51 @@ function render() {
 		}
 		else {	
 			document.getElementById('tooltip').innerHTML = "X : "+INTERSECTED.position.x.toString() +"<br> Y : "+INTERSECTED.position.y.toString()+ "<br> Z : "+INTERSECTED.position.z.toString();
-			
-			
+			document.body.style.cursor = "pointer";
 		}
 
 	} 
 				//on leave 
 		else {
 			// highlight
-			if ( INTERSECTED ) {INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+			if (INTERSECTED) {INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
 			//coordinates tooltip
 			document.getElementById('tooltip').style.display = 'none';	
-			myMarble.removeOrbit(INTERSECTED.orbit);
+			if (!INTERSECTED.selected) myMarble.removeOrbit(INTERSECTED.orbit);
 		}
 		INTERSECTED = null;
+		document.body.style.cursor = "default";
 
 	}
 
 
 }
 window.addEventListener( 'mousemove', onMouseMove, false );
+window.addEventListener("click",() =>{
+	if (INTERSECTED) {
+		if (ACTIVE && ACTIVE != INTERSECTED) {ACTIVE.selected = false;myMarble.removeOrbit(ACTIVE.orbit);}
+		ACTIVE = INTERSECTED;
+		console.log(ACTIVE.color);
+				INTERSECTED.selected = true;
+			}
+	}
+ );
 
 render();
 
 function onWindowResize() {
 
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
+	myMarble.camera.aspect = window.innerWidth / window.innerHeight;
+	myMarble.camera.updateProjectionMatrix();
 
-	renderer.setSize( window.innerWidth, window.innerHeight );
+	myMarble.renderer.setSize( window.innerWidth, window.innerHeight );
 
 }
 window.addEventListener( 'resize', onWindowResize );
 
 function modelCoords(realCoords){
 	var x,y,z;
-	var prop =  (realCoords[2]/6371)*20;
+	var prop =  ((realCoords[2]+6371)/6371)*20;
 	y= Math.sin(realCoords[0]) * prop;
 	var intr = Math.abs(Math.cos(realCoords[0]) * prop);
 	x= intr * Math.cos(realCoords[1]+0.015);
@@ -354,7 +248,20 @@ function modelCoords(realCoords){
 return(new THREE.Vector3(x,y,z));
 }
 
-
+$(document).ready(
+	function(){
+	  $('.spinner').click(
+		function(){
+		$('.main').animate({
+		  left: '-1500px',
+		  opacity: '0',
+		},2000);
+		$('#menu-btn').show(2000);
+		myMarble.entrance();
+	  }
+	  );          
+	}
+  );
 /*
 //satellite import
 var my_satellite;
@@ -417,15 +324,3 @@ var satellite_angle =   0.00;
 
 }
 follow_debris(); */
-
-(async () => {
-	const debris = await fetchRandomDebris(30)
-	const coords = await fetchRandomDebrisCoords(debris)
-	coords.forEach(coord => addDebris(coord.latitude, coord.longitude, coord.height))
-	for (const coord of coords) {
-		try {
-			addDebris(coord.latitude, coord.longitude, coord.height)
-		} catch (e) { }
-	}
-
-})()
